d81c82fbb6dacc1b95c112bf69de6f784ecfd3b9
Oyebamiji Alex <alexferanmi390@gmail.com>
2023-02-16 12:51:56 +0100

    feat: useEffect cleanup

--- src/components/InputMovie.tsx
+++ src/components/InputMovie.tsx
@@ -1,69 +1,78 @@
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import randomMovie from "../utils/randomMovie";
 import Movies from "./Movies";
 import Spinner from "./Spinner";
-const InputMovie = () => {
-    const [text, setText] = useState<string>("")
-    const [data, setData] = useState(randomMovie())
-    const [results, setResults] = useState([])
-    const [loading, setLoading] = useState<boolean>(false)
-    const [error, setError] = useState<string>("")
 
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault()
-        setData(text)
-        setText("")
+const InputMovie = () => {
+  const [text, setText] = useState<string>("");
+  const [data, setData] = useState(randomMovie());
+  const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    setData(text);
+    setText("");
+  };
+
+  const MOVIE_KEY = import.meta.env.VITE_MOVIE_KEY;
+
+  const url = `https://www.omdbapi.com/?s=${data}&apikey=${MOVIE_KEY}`;
+
+  useEffect(() => {
+    const abortCont = new AbortController();
+    async function fetchData() {
+      setLoading(true);
+      try {
+        const response = await fetch(url, { signal: abortCont.signal });
+        const movieData = await response.json();
+        setResults(movieData["Search"]);
+        console.log(results);
+        if (!movieData.ok) {
+          throw new Error(movieData.Error);
+        }
+      } catch (err) {
+        if (err instanceof Error) {
+          setError(err.message);
+          if (err.name === "AbortError") {
+            console.log("successfully aborted");
+          }
+        }
+      } finally {
+        setLoading(false);
+      }
     }
 
-    const MOVIE_KEY = import.meta.env.VITE_MOVIE_KEY
+    fetchData();
+    return () => abortCont.abort();
+  }, [data]);
 
-    const url = `https://www.omdbapi.com/?s=${data}&apikey=${MOVIE_KEY}`
+  return (
+    <section className="mt-6 mx-auto w-4/5 lg:w-3/6">
+      <form
+        onSubmit={handleSubmit}
+        action="submit"
+        className="flex w-full gap-2 justify-center"
+      >
+        <input
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setText(e.target.value)
+          }
+          value={text}
+          type="text"
+          className="border-green-600 border-[1px] rounded-md p-2 outline-none md:flex-1"
+        />
+        <button className="bg-green-600 hover:bg-green-800 text-white p-2 rounded-md">
+          Search
+        </button>
+      </form>
 
-    useEffect(() => {
-        async function fetchData() {
-            setLoading(true)
-            try {
-                const response = await fetch(url)
-                const movieData = await response.json()
-                setResults(movieData['Search'])
-                if (!movieData.ok) {
-                    throw new Error(movieData.Error)
-                }
+      {loading ? <Spinner /> : results && <Movies results={results} />}
 
-            } catch (err) {
-                if (err instanceof Error) {
-                    setError(err.message)
-                }
-            } finally {
-                setLoading(false)
-            }
-        }
-
-        fetchData()
-    }, [data])
-    return (
-        <section className="mt-6 mx-auto w-4/5 lg:w-3/6">
-            <form
-                onSubmit={handleSubmit}
-                action="submit"
-                className="flex w-full gap-2 justify-center"
-            >
-                <input
-                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => setText(e.target.value)}
-                    value={text}
-                    type="text"
-                    className="border-green-600 border-[1px] rounded-md p-2 outline-none md:flex-1"
-                />
-                <button className="bg-green-600 hover:bg-green-800 text-white p-2 rounded-md">
-                    Search
-                </button>
-            </form>
-
-            {error && <div className="font-bold text-center">{error}</div>}
-
-            {loading ? <Spinner />: results && <Movies results={results} />}
-        </section>
-    );
+      {error && <div className="font-bold text-center">{error}</div>}
+    </section>
+  );
 };
 
 export default InputMovie;
--- src/components/Movies.tsx
+++ src/components/Movies.tsx
@@ -1,22 +1,30 @@
 interface Props {
-    results: any;
+  results: List[];
 }
-const Movies = ({results}: Props) => {
-    return (
-        <div className="py-8 grid gap-6 grid-cols-1 xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-3 mb-8 md:gap-8">
-            {results && results.map((result: any, index: number) => (
-                <div key={index}
-                >
-                    <img src={result.Poster} alt="Movie Poster"/>
-                    <div>
-                        <h1>{result.Title}</h1>
-                        <p>{result.Year}</p>
-                        <p>{result.Type}</p>
-                    </div>
-                </div>
-            ))}
-        </div>
-    );
+
+interface List {
+  Poster: string;
+  Title: string;
+  Year: number;
+  Type: string;
+}
+
+const Movies = ({ results }: Props) => {
+  return (
+    <div className="py-8 grid gap-6 grid-cols-1 xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-3 mb-8 md:gap-8">
+      {results &&
+        results.map((result, index) => (
+          <div key={index}>
+            <img src={result.Poster} alt="Movie Poster" />
+            <div>
+              <h1>{result.Title}</h1>
+              <p>{result.Year}</p>
+              <p>{result.Type}</p>
+            </div>
+          </div>
+        ))}
+    </div>
+  );
 };
 
 export default Movies;
